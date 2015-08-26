05003e296527dd78289ff3541b9bf5077c5b04a4
cleestofuh <cleestofuh@CHRISTOPHER-MACBOOK-PRO-YO.local>
2015-08-26 13:03:09 -0700

    updated form

--- js/create_group.js
+++ js/create_group.js
@@ -1,12 +1,12 @@
-
 $(function() {
 
   Parse.$ = jQuery;
 
   // Replace this line with the one on your Quickstart Guide Page
   Parse.initialize("0s0tCOHzCaM72KQpBBEoCbP0b3f8ft7TzfNF9DzU", "veqqdyOSdNttB7IsKmvMHrpsZSQGnCoNSCJQ45zN");
-
-  $("#index").submit(function(event) {
+  var Group = new Parse.Object.extend("Group");
+  
+  $("#create-group").submit(function(event) {
 
     var className = $("class-name").val();
     var professor = $("teacher-name").val();
@@ -16,7 +16,6 @@ $(function() {
     var location = $("location").val();
     var description = $("desc").val();
 
-    var Group = new Parse.Object.extend("Group");
     var group = new Group();
 
     group.set("class_name", className);
@@ -27,4 +26,18 @@ $(function() {
     group.set("location", location);
     group.set("description", description);
 
+    group.save(null, {
+      success: function(group) {
+        // Execute any logic that should take place after the object is saved.
+        alert('New object created with objectId: ' + group.id);
+      },
+      error: function(error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        alert('Failed to create new object, with error code: ' + error.message);
+      }
+    });
+
   });
+
+});
