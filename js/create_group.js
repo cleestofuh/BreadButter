cbfd47b688e899ab64de1ace3a2ec48386f8a373
cleestofuh <cleestofuh@CHRISTOPHER-MACBOOK-PRO-YO.local>
2015-08-26 14:31:25 -0700

    creating a class working

--- js/create_group.js
+++ js/create_group.js
@@ -4,26 +4,27 @@ $(function() {
 
   // Replace this line with the one on your Quickstart Guide Page
   Parse.initialize("0s0tCOHzCaM72KQpBBEoCbP0b3f8ft7TzfNF9DzU", "veqqdyOSdNttB7IsKmvMHrpsZSQGnCoNSCJQ45zN");
-  var Group = new Parse.Object.extend("Group");
-  
-  $("#create-group").submit(function(event) {
 
-    var className = $("class-name").val();
-    var professor = $("teacher-name").val();
-    var startTime = $("time-start").val();
-    var endTime = $("time-end").val();
-    var date = $("date").val();
-    var location = $("location").val();
-    var description = $("desc").val();
+  $("#createGroup").submit(function(event) {
+    event.preventDefault();
 
+    var className = $("#class-name").val();
+    var teacher = $("#teacher-name").val();
+    var location = $("#location").val();
+    var startTime = $("#time-start").val().toString();
+    var endTime = $("#time-end").val().toString();
+    var date = $("#date").val().toString();
+    var description = $("#desc").val();
+
+    var Group = Parse.Object.extend("Group");
     var group = new Group();
 
     group.set("class_name", className);
-    group.set("professor", professor);
+    group.set("teacher", teacher);
+    group.set("location", location);
     group.set("startTime", startTime);
     group.set("endTime", endTime);
     group.set("date", date);
-    group.set("location", location);
     group.set("description", description);
 
     group.save(null, {
