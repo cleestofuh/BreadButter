26a853ee99eb9bf60760e99df7182b665628f87d
cleestofuh <cleestofuh@CHRISTOPHER-MACBOOK-PRO-YO.local>
2015-08-30 15:55:27 -0700

    added functionality so that you can only create a group if you are in that class

--- js/create_group.js
+++ js/create_group.js
@@ -22,64 +22,64 @@ $(function() {
     var studyBuddy = currentUser.get("firstName");
 
 // only able to create group if you are in the class
-/*    var Class = Parse.Object.extend("Class");
-    var query = new Parse.Query(Class);
-    query.include("class-name");
-    query.find({
+    var Class = Parse.Object.extend("Class");
+    var createQuery = new Parse.Query(Class);
+    createQuery.include("class-name");
+    createQuery.find({
       success: function(classes) {
         for(var i = 0; i < classes.length; i++) {
           if("#class-name" == classes[i]) {
             // save
+            // create a new group object
+            var Group = Parse.Object.extend("Group");
+            var group = new Group();
+
+            // set fields in group object
+            group.set("username", userName);
+            group.set("class_name", className);
+            group.set("teacher", teacher);
+            group.set("location", location);
+            group.set("startTime", startTime);
+            group.set("endTime", endTime);
+            group.set("date", date);
+            group.set("description", description);
+            group.set("studybuddy", studyBuddy);
+
+            // save the group
+            group.save(null, {
+              success: function(group) {
+                // Execute any logic that should take place after the object is saved.
+                alert('New object created with objectId: ' + group.id);
+              },
+              error: function(error) {
+                // Execute any logic that should take place if the save fails.
+                // error is a Parse.Error with an error code and message.
+                alert('Failed to create new object, with error code: ' + error.message);
+              }
+            });
+
+            // add the group to the groupArray in the user who created it
+            currentUser.add("createdGroups", group);
+            currentUser.save(null, {
+              success: function(currentUser) {
+                // Execute any logic that should take place after the object is saved.
+                alert('SUCCESS');
+              },
+              error: function(error) {
+                // Execute any logic that should take place if the save fails.
+                // error is a Parse.Error with an error code and message.
+                alert('FAILURE');
+              }
+            });
+
           } else {
             // you are not in this class
+            alert('You are not in that class');
           }
 
         }
       }
-    });*/
-
-    // create a new group object
-    var Group = Parse.Object.extend("Group");
-    var group = new Group();
-
-    // set fields in group object
-    group.set("username", userName);
-    group.set("class_name", className);
-    group.set("teacher", teacher);
-    group.set("location", location);
-    group.set("startTime", startTime);
-    group.set("endTime", endTime);
-    group.set("date", date);
-    group.set("description", description);
-    group.set("studybuddy", studyBuddy);
-
-    // save the group
-    group.save(null, {
-      success: function(group) {
-        // Execute any logic that should take place after the object is saved.
-        alert('New object created with objectId: ' + group.id);
-      },
-      error: function(error) {
-        // Execute any logic that should take place if the save fails.
-        // error is a Parse.Error with an error code and message.
-        alert('Failed to create new object, with error code: ' + error.message);
-      }
     });
-
-    // add the group to the groupArray in the user who created it
-    currentUser.add("createdGroups", group);
-    currentUser.save(null, {
-      success: function(currentUser) {
-        // Execute any logic that should take place after the object is saved.
-        alert('SUCCESS');
-      },
-      error: function(error) {
-        // Execute any logic that should take place if the save fails.
-        // error is a Parse.Error with an error code and message.
-        alert('FAILURE');
-      }
-    });
-
 
   });
 
