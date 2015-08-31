dcdd4399a1e21542a306f838eddbf94b7aaa85f7
cleestofuh <cleestofuh@CHRISTOPHER-MACBOOK-PRO-YO.local>
2015-08-31 14:04:52 -0700

    my groups

--- js/my_groups.js
+++ js/my_groups.js
@@ -4,5 +4,22 @@ $(function() {
 
   Parse.initialize("0s0tCOHzCaM72KQpBBEoCbP0b3f8ft7TzfNF9DzU", "veqqdyOSdNttB7IsKmvMHrpsZSQGnCoNSCJQ45zN");
 
+  var myGroups = Parse.Object.extend("Groups");
+  var query = new Parse.Query(myGroups);
+  var currentUser = Parse.User.current();
+  var curUsername = currentUser.get("username");
+  query.get("98uP6x97Ki", {
+    success: function(object) {
+      $('#myGroups-table').append('<tr>' +'<td>' + object.get('classNames') + '</td>'
+                                         +'<td>' + object.get('teacher') + '</td>'
+                                         +'<td>' + object.get('date') + '</td>'
+                                         +'<td>' + object.get('startTime') + ' - ' + object.get('endTime')
+                                         +'<td>' + object.get('location') + '</td>' + '</td>'
+                                         +'<td>' + object.get('studybuddy') + '</td>'
+                                         +'<td>' + object.get('description') + '</td>' + '<tr>');
+    }
+  })
+*/
+
 
 });
